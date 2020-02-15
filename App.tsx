f3dd89330ffd1e02e2669107632ee3652f20fba1
imalbert <albert.s.manuel@gmail.com>
2020-02-15 14:02:16 +0800

    Add rn-paper & react-navigation drawer

--- App.tsx
+++ App.tsx
@@ -1,12 +1,64 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
 
-export default function App() {
+import { Provider as PaperProvider } from 'react-native-paper'
+import { NavigationContainer } from '@react-navigation/native'
+
+import { StyleSheet, Text, View } from 'react-native'
+import { Portal, FAB } from 'react-native-paper'
+
+import { createDrawerNavigator, useIsDrawerOpen } from '@react-navigation/drawer'
+
+const Drawer = createDrawerNavigator()
+
+const Wallet = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
-  );
+  )
+}
+
+const Records = () => {
+  const showFAB = !useIsDrawerOpen()
+
+  return (
+    <>
+    <View style={styles.container}>
+      <Text>Transaction Records</Text>
+    </View>
+    <Portal>
+      {showFAB && <FAB
+        icon="feather"
+        style={{
+          position: 'absolute',
+          bottom: 16,
+          right: 16,
+        }}
+      />}
+    </Portal>
+    </>
+  )
+}
+
+const RootNavigator = () => {
+  return (
+    <>
+    <Drawer.Navigator drawerContent={() => <Wallet />}>
+      <Drawer.Screen name="Records" component={Records} />
+    </Drawer.Navigator>
+    
+    </>
+  )
+}
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </PaperProvider>
+  )
 }
 
 const styles = StyleSheet.create({
