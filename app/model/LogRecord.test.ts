4356c02c0d096d508db33a3e03b79ada1e440b1d
imalbert <albert.s.manuel@gmail.com>
2020-02-16 11:38:37 +0800

    Add Log/Record models + tests

--- app/model/LogRecord.test.ts
+++ app/model/LogRecord.test.ts
@@ -1,15 +1,106 @@
-import { LogRecord } from './LogRecord'
+import {
+  getSnapshot, IJsonPatch, onPatch,
+} from 'mobx-state-tree'
 
-describe('LogRecord Model', () => {
-  const dateTest = new Date()
+import {
+  Log,
+  Record,
+  LogModelType,
+  LOG_TYPES,
+  LOG_CATEGORY,
+  RECORD_ACCOUNT,
+  RecordModelType,
+} from './LogRecord'
 
+const DEFAULT_DATE = new Date(1581819204151)
+const DEFAULT_BALANCE = 1000
+const DEFAULT_ACCOUNT = 'CASH_IN_HAND'
+const DEFAULT_LOG_AMOUNT = 500
+const DEFAULT_LOG_CATEGORY = LOG_CATEGORY.FOOD
+const DEFAULT_LOG_TYPE = LOG_TYPES.EXPENSE
+const buildLogObj = (overrides?): LogModelType => ({
+  amount: DEFAULT_LOG_AMOUNT,
+  category: DEFAULT_LOG_CATEGORY,
+  date: DEFAULT_DATE,
+  type: DEFAULT_LOG_TYPE,
+  ...overrides,
+})
+
+describe('Log Model', () => {
+  
   it('can create an instance', () => {
-    LogRecord.create({
-      amount: 123,
-      category: 'Food',
-      date: dateTest,
-      type: 'Expense',
+    const log = Log.create(buildLogObj())
+    
+    expect(getSnapshot(log)).toMatchSnapshot()
+  })
+  
+  describe('actions', () => {
+    let log: LogModelType
+    let patches: Array<IJsonPatch>
+  
+    beforeEach(() => {
+      patches = []
+      log = Log.create(buildLogObj())
+  
+      onPatch(log, snapshot => patches.push(snapshot))
+    })
+    afterEach(() => { expect(patches).toMatchSnapshot() })
+
+    it('can update a log', () => {
+      log.changeAmount(999)
+      log.changeCategory(LOG_CATEGORY.HOUSEHOLD)
+      log.changeDate(new Date(1581819204152))
+      log.changeType(LOG_TYPES.INCOME)
+
+      expect(getSnapshot(log)).toMatchSnapshot()
     })
   })
+})
 
+describe('Records Model', () => {
+  it('can create a log record with defaults', () => {
+    const record_with_defaults = Record.create()
+
+    expect(getSnapshot(record_with_defaults)).toMatchSnapshot()
+  })
+
+  it('can create a log record with defaults', () => {
+    const record_with_props = Record.create({
+      balance: DEFAULT_BALANCE,
+      logs: [Log.create(buildLogObj())],
+    })
+
+    expect(getSnapshot(record_with_props)).toMatchSnapshot()
+  })
+
+  describe('actions', () => {
+    let record: RecordModelType
+    let patches: Array<IJsonPatch>
+
+    beforeEach(() => {
+      patches = []
+      record = Record.create({ balance: DEFAULT_BALANCE })
+
+      onPatch(record, snapshot => patches.push(snapshot))
+    })
+    afterEach(() => { expect(patches).toMatchSnapshot() })
+
+    it('can record a log', () => {
+      record.add(Log.create(buildLogObj()))
+      record.add(Log.create(buildLogObj({ type: LOG_TYPES.INCOME })))
+    })
+
+    it('can remove all logs', () => {
+      record.add(Log.create(buildLogObj()))
+      record.add(Log.create(buildLogObj()))
+      const totalExpenses = DEFAULT_LOG_AMOUNT * 2
+
+      expect(record.logs.length).toBe(2)
+      expect(record.balance).toBe(DEFAULT_BALANCE - totalExpenses)
+
+      record.clear()
+      expect(record.logs.length).toBe(0)
+      expect(record.balance).toBe(DEFAULT_BALANCE)
+    })
+  })
 })
--- app/model/LogRecord.ts
+++ app/model/LogRecord.ts
@@ -1,10 +1,78 @@
-import { types } from 'mobx-state-tree'
+import {
+  types,
+  getParent,
+  destroy,
+  Instance,
+  SnapshotIn,
+} from 'mobx-state-tree'
 
-export const LogRecord = types
-  .model('LogRecord')
+export enum LOG_TYPES { EXPENSE = 'EXPENSE', INCOME = 'INCOME' }
+export enum LOG_CATEGORY { FOOD = 'FOOD', HOUSEHOLD = 'HOUSEHOLD' }
+export enum RECORD_ACCOUNT { CASH_IN_HAND = 'CASH_IN_HAND' }
+export enum RECORD_ACTIONS { ADD = 'RECORD_ADD', RM = 'RECORD_RM' }
+
+export const Log = types.model('Log')
   .props({
     amount: types.number,
     category: types.string,
     date: types.Date,
     type: types.string,
   })
+  .views(self => ({}))
+  .actions(self => ({
+    changeAmount(newAmount: number) { self.amount = newAmount },
+    changeCategory(newCategory: LOG_CATEGORY) {
+      self.category = newCategory
+    },
+    changeDate(newDate: Date) { self.date = newDate },
+    changeType(newType: LOG_TYPES) { self.type = newType },
+    remove() {
+      getParent(self, 2).remove(self)
+    }
+  }))
+
+const isExpense = (log: LogModelType) => log.type === LOG_TYPES.EXPENSE
+const calcBalance = (
+  log:     LogModelType, 
+  balance: number, 
+  action:  RECORD_ACTIONS): number => {
+  switch(action) {
+    case RECORD_ACTIONS.ADD: {
+      return isExpense(log)
+        ? balance - log.amount
+        : balance + log.amount
+    }
+    case RECORD_ACTIONS.RM: {
+      return isExpense(log)
+        ? balance + log.amount
+        : balance - log.amount
+    }
+    default:
+      return balance
+  }
+}
+
+export const Record = types.model('Record')
+  .props({
+    account: types.optional(types.string, RECORD_ACCOUNT.CASH_IN_HAND),
+    balance: types.optional(types.number, 0),
+    logs: types.optional(types.array(Log), []),
+  })
+  .actions(self => {
+    return {
+      add(log: LogModelType) {
+        self.logs.push(log)
+        self.balance = calcBalance(log, self.balance, RECORD_ACTIONS.ADD)
+      },
+      remove(log: LogModelType) {
+        self.balance = calcBalance(log, self.balance, RECORD_ACTIONS.RM)
+        destroy(log)
+      },
+      clear() {
+        self.logs.forEach((log) => log.remove())
+      },
+    }
+  })
+
+export type RecordModelType = Instance<typeof Record>
+export type LogModelType = Instance<typeof Log>
